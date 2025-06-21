d7eb1c1004500fe9457e7925d52c2d0f3b29a482
LubenStefano <lubenstefanovkn@gmail.com>
2025-06-21 04:30:01 +0300

    Refactor AddEventButton integration and update styles in Index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import AddEventButton from "@/src/components/AddEventButton";
 import MainCalendar from "@/src/components/mainCalendar";
 import {Button, StyleSheet, Text, View } from "react-native";
 
@@ -5,9 +6,10 @@ export default function Index() {
   return (
     <View style={styles.container} >
     <MainCalendar />
-    <View style={styles.button}>
-      <Button title="ADD EVENT" onPress={() => alert("Add Event Pressed")} color={"000000"} />
-    </View>
+    <AddEventButton
+      onPress={() => {
+        console.log("Add Event Button Pressed");
+      }} />
     </View>
   );
 }
@@ -18,21 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#ffffff",
-  },
-  button: {
-    position: "absolute",
-    top: 510,
-    width: "90%",
-    padding: 10,
-    backgroundColor: "#ffbd59",
-    borderRadius: 30,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
   },
 });
--- src/components/AddEventButton.tsx
+++ src/components/AddEventButton.tsx
@@ -16,29 +16,34 @@ export default function AddEventButton({ onPress, style }: AddEventButtonProps) 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff7e6',
-    borderRadius: 20,
-    boxShadow: '0px 2px 8px rgba(0,0,0,0.15)',
-    padding: 12,
-    position: 'absolute',
-    top: 20,
-    right: 20,
-    width: 140,
-    zIndex: 100,
-  },
-  button: {
-    backgroundColor: '#ffbd59',
-    borderRadius: 16,
-    paddingVertical: 10,
-    paddingHorizontal: 16,
-    alignItems: 'center',
-  },
-  text: {
-    color: '#7a4c15',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    textTransform: 'uppercase',
-    fontSize: 16,
-  },
+    
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+    },
+    button: {
+        position: "absolute",
+        top: 510,
+        padding: 20,
+        width: "90%",
+        backgroundColor: "#ffbd59",
+        borderRadius: 30,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        alignItems: 'center',
+    },
+    text: {
+        color: '#7a4c15',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontSize: 18,
+    },
 });
